14ad0e9d75a283f50dac688a46dd4cd354baa433
Vgrams05 <virginia.grams@yahoo.com>
2021-01-28 18:24:57 -0800

    new packages

---
+++ lambdata/helper_functions.py
@@ -0,0 +1,13 @@
+"""A collection of Data Science helper functions"""
+
+# accessing libraries through pipenv
+import pandas as pd
+import numpy as np
+
+
+
+def null_count(df):
+    """Check a dataframe for nulls and return the number of missing values"""
+    # TODO - implement null_count(df)
+    return pd.isna()
+    
