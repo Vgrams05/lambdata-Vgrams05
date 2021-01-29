0390c5b8ed705867b150200fc1635706b6926214
Vgrams05 <virginia.grams@yahoo.com>
2021-01-28 19:19:34 -0800

    new packages

--- lambdata/helper_functions.py
+++ lambdata/helper_functions.py
@@ -7,7 +7,18 @@ import numpy as np
 
 
 def null_count(df):
-    """Check a dataframe for nulls and return the number of missing values"""
-    # TODO - implement null_count(df)
-    return pd.isna()
+    """ Check a dataframe for nulls and return the number of missing values."""
     
+    null_count = [df.isnull().sum().sum()]
+    return null_count
+    
+
+ def randomize(df, seed):
+     """Develop a randomization function that randomizes all of a dataframes cells then returns that randomized dataframe. This function should also take a random seed for reproducible randomization."""
+      for i, x in df.items():
+    print (x)
+    np.random.seed(x)
+    #some random function
+    randomize = np.random.randint(5, size=2)
+    
+    return randomize 
